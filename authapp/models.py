1d9c42d2dd0e2571f1a50ad0be71b4296dfb0884
brain4power <yotakrasnodar@yandex.ru>
2019-08-21 20:06:21 +0300

    backend: add docstring to user model

--- authapp/models.py
+++ authapp/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 
 class SimpleUser(AbstractUser):
+    """Just user model"""
+
     name = models.CharField(max_length=32)
     mail = models.EmailField(max_length=32)
